9511def87e709822b66616196825622166ba4b79
Pe6oProgramista <p.milev3@gmail.com>
2017-12-03 23:38:12 +0200

    zdr

--- ipc3/cyclicRead.c
+++ ipc3/cyclicRead.c
@@ -5,31 +5,37 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include "cyclicBuf.h"
+#include "gen.h"
+#include "gen.c"
 
 int main() {
-	sleep(3);
 	int memFd = shm_open( "simple_memory", O_RDONLY, 0 );
-	if(memFd < 0) {
-		perror("file");
+	if( memFd == -1 ) {
+		perror("Can't open file");
 		return 1;
 	}
 
 	struct cyclic_buf* mem = mmap( NULL, sizeof(struct cyclic_buf), PROT_READ, MAP_SHARED, memFd, 0 );
-	if(mem == NULL) {
-		perror("mmap");
+	if( mem == NULL ) {
+		perror("Can't mmap");
 		return -1;
 	}	
 
 	uint64_t pos = ( mem->pos + 2048 ) % 4096;
 	printf("starting at %ld\n", pos);
-	while( true ) {
+	while(true) {
 		if(mem->pos == pos) {
-			sleep(1);
 			continue;
 		}
-		printf("%d\n", mem->array[pos]);
-		if(mem->array[pos] == 9)
-			printf( "\n" );
+
+		if(verify((void*)mem->array) != mem->pos) {
+			printf("Failed at %ld\n", mem->pos);
+			return 1;
+		}
+		if(mem->pos%1024 == 0) {
+			printf("passed %lu\n", pos);
+		}
+
 		pos++;
 		pos %= 4096;
 	}
